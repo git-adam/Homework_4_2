adde65b5036a6473eda4e9d24af955ae1ad94013
git-adam <awwojcik1@gmail.com>
2022-07-30 11:52:20 +0200

    final solution

--- Homework_4_2/Program.cs
+++ Homework_4_2/Program.cs
@@ -6,10 +6,74 @@ using System.Threading.Tasks;
 
 namespace Homework_4_2
 {
+    public class FizzBuzz 
+    {
+        public FizzBuzz()
+        {
+
+        }
+
+        public string FizzBuzzMethod(int number)
+        {
+            string tmp;
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                tmp = "FizzBuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                tmp = "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                tmp = "Buzz";
+            }
+            else
+            {
+                tmp = number.ToString();
+            }
+
+            return tmp;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("FizzBuzz");
+
+            Console.Write("Podaj liczbę: ");
+            var fizzBuzz = new FizzBuzz();
+
+            while (true)
+            {
+                var number = GetNumber();
+                var result = fizzBuzz.FizzBuzzMethod(number);
+                Console.WriteLine($"Otrzymany wynik: {result}");
+                Console.Write("Aby zakończyć program wciśnij 't' lub podaj nową liczbę: ");
+            }
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                var value = Console.ReadLine();
+
+                if (value.ToUpper() == "T")
+                    Environment.Exit(0);
+
+                if (!int.TryParse(value, out int number))
+                {
+                    Console.WriteLine("Nieprawidłowe dane, spróbuj jeszcze raz.");
+                    continue;
+                }
+
+                return number;
+            }
+
         }
     }
 }
